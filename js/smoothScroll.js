41c6915174849f9a25ea13efd33bbb59bbac1f43
Al Tsaplin <al.tsaplin@gmail.com>
2024-06-07 08:36:54 +0300

    feat(smooth scroll) Add 'seamless-scroll-polyfill' lib to the project for cross-browser smooth auto-scroll

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -6,13 +6,18 @@ const links = navbar.querySelectorAll('a');
 links.forEach(link => {
 	link.addEventListener('click', (evt) => {
 		evt.preventDefault();
-		const section = document.querySelector(link.getAttribute('href')); // получаем секцию, к которой нам нужно прокрутить страницу
-
+		// const section = document.querySelector(link.getAttribute('href')); // получаем секцию, к которой нам нужно прокрутить страницу
+		const anchor = link.getAttribute('href');
 		// Сделаем проверку, если querySelector нашёл секцию, то мы прокрутим к ней
-		if (section) {
-			section.scrollIntoView({
+		if (anchor) {
+			/* section.scrollIntoView({
 				block: 'start',
 				behavior: 'smooth'
+			}); */
+			seamless.scrollIntoView(document.querySelector(anchor), {
+				behavior: 'smooth',
+				block: 'start',
+				inline: 'center',
 			});
 		}
 	})
